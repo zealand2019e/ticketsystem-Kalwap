54ba221a9535d1f2b78591aa8a37f10a623c2c56
Grzegorz Kalwap <kalwap00@gmail.com>
2019-09-18 12:03:11 +0200

    task2

---
+++ TicketLibrary/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketLibrary
+{
+    class Car
+    {
+        /// <summary>
+        /// Car class attribiutes
+        /// </summary>
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+        
+        /// <summary>
+        /// Car class methods contains fixed values
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 240;
+        }
+        
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+        
+        
+
+        
+    }
+}
